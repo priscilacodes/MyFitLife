3db281e16798cb66683f8c61791eea47d2e3298d
Pri-Codes <pri.codes@gmail.com>
2023-05-21 19:14:59 +1200

    Initial Commit

---
+++ MyFitLife/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFitLife
+//
+//  Created by Priscila Oliveira on 21/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFitLife/MyFitLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFitLifeApp.swift
+//  MyFitLife
+//
+//  Created by Priscila Oliveira on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFitLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
